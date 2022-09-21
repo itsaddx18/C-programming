5e55cd99f2ba61c617e0b558f997d3f70d759831
Adnaan Dar <106769069+itsaddx18@users.noreply.github.com>
2022-09-21 14:27:13 +0530

    Create arr_matrix.c

---
+++ arr_matrix.c
@@ -0,0 +1,24 @@
+//making the matrix 
+#include <stdio.h>
+int main()
+{
+    int a[2][2],i,j;
+    printf("Enter array elements :");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Enter matrix elements\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
